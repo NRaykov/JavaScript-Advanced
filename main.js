3dc130e9df19d8ca5ec2d769a12d0ae1f8003a1e
NRaykov <nediotenev@gmail.com>
2019-07-04 20:44:42 +0300

    Memory Heap, Call Stack, Stack Overflow

---
+++ main.js
@@ -0,0 +1,61 @@
+// JavaScript Engine
+function jsengine(code) {
+    return code.split(/\s+/);
+}
+jsengine('var a = 5');
+
+
+//Inline caching
+function findUser(user) {
+    return `found ${user.firstName} ${user.lastName}`;
+}
+
+const userData = {
+    firstName: 'Max',
+    lastName: 'Junior'
+};
+
+findUser(userData);
+
+//Hidden classes
+function Animal(x,y) {
+    this.x = x;
+    this.y = y;
+}
+
+const obj1 = new Animal(1,2);
+const obj2 = new Animal(4,5);
+
+//Call stack & Memory Heap
+const number = 610; //allocate memory for primitive type 'number'
+const string = 'some text'; //allocate memory fot primitive type 'string'
+
+const obj = { //allocate memory for an object ant it's values
+    firtName: 'Nedyalko',
+    lastName: 'Raykov'
+};
+
+
+//Call stack
+function substact(num) {
+    return num - 2;
+
+}
+
+function calculate() {
+    const sumTotal = 4 + 5;
+
+    return substact(sumTotal); // subtract 9 - 2 = 7
+}
+
+calculate(); // Call stack stores functions and variables
+
+
+//Stack overflow
+
+function inception() {
+    inception();
+}
+//inception();
+// VM141:1 Uncaught RangeError: Maximum call stack size exceeded
+
